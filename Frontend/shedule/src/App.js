6c2c93b836493f43acecd096c53404026c2980f6
Artem Troy <coupdegr@gmail.com>
2022-04-19 12:41:12 +0300

    Настроенное реакт приложение

---
+++ Frontend/shedule/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Typography>
+          Wow
+        </Typography>
+      </header>
+    </div>
+  );
+}
+
+export default App;
